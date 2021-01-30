5cc8e489f1b0072c5e82516ae92a983cdefea20b
Chris <chrsdng5@gmail.com>
2021-01-30 10:51:50 -0800

    Troll tracks, moves towards the play and gets faster as it does so

--- Assets/Scripts/TrollBehavior.cs
+++ Assets/Scripts/TrollBehavior.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class TrollBehavior : MonoBehaviour, GenericKillableEntity
 {
+    GameObject Target;
+    public float speed;
+    private float step;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Target = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
+        float dist = Vector2.Distance(transform.position, Target.GetComponent<Transform>().position);
+        if (dist > .001f) {
+            step = speed / dist * Time.deltaTime;
+            transform.position = Vector2.MoveTowards(transform.position, Target.GetComponent<Transform>().position, step);
+        }
         
     }
 
