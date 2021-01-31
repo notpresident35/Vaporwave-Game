f5359b2c4e635fe987d332985469facd3d7bb31c
Chris <chrsdng5@gmail.com>
2021-01-30 17:39:34 -0800

    TomoThots now multiply

---
+++ Assets/Scripts/TomoAkbar.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations;
+
+public class TomoAkbar : MonoBehaviour
+{
+    public GameObject moreTomo;
+    public Animator TomoEggs;
+    public int numTomosSpawned;
+    public float timeTilDeath;
+    public float spawnForce;
+    private float timer;
+    private TomogachiMovement tm;
+    private Rigidbody2D rb;
+    void Start()
+    {
+        tm = GetComponent<TomogachiMovement>();
+        rb = GetComponent<Rigidbody2D>();
+        timer = timeTilDeath;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        if (timer <= 0) {
+            spawnNewTomos();
+        }
+    }
+
+    void spawnNewTomos() {
+        tm.enabled = false;
+        rb.velocity = Vector2.zero;
+        TomoEggs.SetTrigger("Start");
+    }
+
+    void BlowUp() {
+        Rigidbody2D clone;
+        for (int i = 0; i < numTomosSpawned; i++) {
+            clone = Instantiate(moreTomo, transform.position + new Vector3(Random.Range(0f, 1f), Random.Range(0f, 1f),0), transform.rotation).GetComponent<Rigidbody2D>();
+            clone.velocity = transform.TransformDirection(new Vector3(Random.Range(0f, 1f), Random.Range(0f, 1f), 0) * spawnForce);
+        }
+        Destroy(gameObject);
+    }
+}
--- Assets/Scripts/TomogachiMovement.cs
+++ Assets/Scripts/TomogachiMovement.cs
@@ -7,17 +7,22 @@ public class TomogachiMovement : MonoBehaviour
     public float speed;
     public float upperBound;
     public float lowerBound;
-
+    public float waitTimer;
+    private float timer;
     Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        timer = waitTimer;
     }
 
     // Update is called once per frame
     void Update()
     {
-        rb.velocity = new Vector2(Random.Range(lowerBound, upperBound),Random.Range(lowerBound, upperBound));
+        timer -= Time.deltaTime;
+        if (timer <= 0) {
+            rb.velocity = new Vector2(Random.Range(lowerBound, upperBound), Random.Range(lowerBound, upperBound));
+        }
     }
 }
