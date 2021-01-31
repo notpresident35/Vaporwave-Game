3069faf80a945333663cc56b3b4e5074c1686684
notpresident35 <notpresident35@gmail.com>
2021-01-30 17:39:34 -0800

    CRT Shenanigans
    Crank Assets/Custom_CRT -> ResolutionScale up to the max for an atari game :)

--- Assets/Scripts/CRTTest.cs
+++ Assets/Scripts/CRTTest.cs
@@ -7,9 +7,9 @@ public class CRTTest : MonoBehaviour {
  
     public Material material;
     // Use this for initialization
-    void Start () {
+    /*void Start () {
         material = new Material (Shader.Find ("Hidden/CRT"));
-    }
+    }*/
 
     public void OnRenderImage (RenderTexture source, RenderTexture destination) {
         material.SetTexture ("_MainTex", source);
