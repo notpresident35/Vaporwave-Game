5326e484e314a2865f7b260c374fceae20477992
Chris <chrsdng5@gmail.com>
2021-01-30 20:41:02 -0800

    You can now bonk tamaThots

--- Assets/Scripts/TomoAkbar.cs
+++ Assets/Scripts/TomoAkbar.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Animations;
 
-public class TomoAkbar : MonoBehaviour
+public class TomoAkbar : MonoBehaviour,GenericKillableEntity
 {
     public GameObject moreTomo;
     public Animator TomoEggs;
@@ -40,7 +40,15 @@ public class TomoAkbar : MonoBehaviour
         for (int i = 0; i < numTomosSpawned; i++) {
             clone = Instantiate(moreTomo, transform.position + new Vector3(Random.Range(0f, 1f), Random.Range(0f, 1f),0), transform.rotation).GetComponent<Rigidbody2D>();
             clone.velocity = transform.TransformDirection(new Vector3(Random.Range(0f, 1f), Random.Range(0f, 1f), 0) * spawnForce);
+            DoorController.EnemySpawned();
         }
+        DoorController.EnemyDied();
+        Destroy(gameObject);
+    }
+
+    public void Die()
+    {
+        DoorController.EnemyDied();
         Destroy(gameObject);
     }
 }
