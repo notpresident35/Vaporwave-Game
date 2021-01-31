7d52db834359a1c79abdcfcdf0643e7eb422eb06
Avineesh Kompella <AvineeshKom@gmail.com>
2021-01-31 14:09:38 -0800

    title scene

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
